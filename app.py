b7890f73466e5cf8e1b0923853fbf7056483d2fc
SAYALI <sayalip1506@gmail.com>
2025-06-09 17:50:15 +0530

    user interaction via terminal enabled

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
+# Load environment variables from .env file
 load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
 
-# Test request
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "Say hello!"}]
-)
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
-# Print response
-print(response.choices[0].message.content)
+while True :
+    user=input()
+    
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[         
+            {"role": "user", "content": user}]
+    )
+
+    # Print response
+    print("GPT:", response.choices[0].message.content)
