bb2e7a40012eac3c87f2d8656e7b7f88d2db97dd
SAYALI <sayalip1506@gmail.com>
2025-06-11 16:40:44 +0530

    small change made in user input

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
 while True :
-    user=input()
+    user=input("User: ")
     
     response = client.chat.completions.create(
         model="gpt-4o-mini",
