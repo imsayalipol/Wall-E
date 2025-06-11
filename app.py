4125820e2b2ea784ca6233e5a4e054dec0179b6c
SAYALI <sayalip1506@gmail.com>
2025-06-11 18:27:57 +0530

    Read already uploaded PDF file and staying in interactive loop

--- app.py
+++ app.py
@@ -2,34 +2,34 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
-
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
+# Upload the file only once
 file = client.files.create(
     file=open("Alright.pdf", "rb"),
     purpose="user_data"
 )
-
 file_id = file.id
 
-while True :
-    user=input("User: ")
-    
-    response = client.chat.completions.create(
+while True:
+    user = input("User: ")
+        
+    response = client.responses.create(
         model="gpt-4o-mini",
-        messages=[         
+        input=[
             {
-                "role": "user", 
+                "role": "user",
                 "content": [
-                    { "type":  "input_file", "file_id": file.id},
-                    { "type": "input_text", "text": user}
+                    {"type": "input_file", "file_id": file_id},
+                    {"type": "input_text", "text": user}
                 ]
             }
         ]
     )
 
-    # Print response
-    print("GPT:", response.choices[0].message.content)
+    # Print the response
+    print("GPT:", response.output_text)
+    # print("GPT:", response.choices[0].message.content)
