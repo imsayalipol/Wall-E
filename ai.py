c11fced6e77b71eea0e3f6780e4d04e811770b5b
SAYALI <sayalip1506@gmail.com>
2025-06-24 10:55:34 +0530

    css file added, route created to integrate with ai

---
+++ ai.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+# Load environment variables
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+# Upload the file only once
+file = client.files.create(
+    file=open("DK_Space_Visual_Encyclopedia.pdf", "rb"),
+    purpose="user_data"
+)
+file_id = file.id
+
+while True:
+    user = input("User: ")
+        
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "input_file", "file_id": file_id},
+                    {"type": "input_text", "text": user}
+                ]
+            }
+        ]
+    )
+
+    # Print the response
+    print("GPT:", response.output_text)
--- app.py
+++ app.py
@@ -1,35 +1,14 @@
-from dotenv import load_dotenv
-import os
-from openai import OpenAI
+from flask import Flask, render_template, redirect, request
 
-# Load environment variables
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+app = Flask(__name__)
 
-# Upload the file only once
-file = client.files.create(
-    file=open("Alright.pdf", "rb"),
-    purpose="user_data"
-)
-file_id = file.id
 
-while True:
-    user = input("User: ")
-        
-    response = client.responses.create(
-        model="gpt-4o-mini",
-        input=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "input_file", "file_id": file_id},
-                    {"type": "input_text", "text": user}
-                ]
-            }
-        ]
-    )
-
-    # Print the response
-    print("GPT:", response.output_text)
-    # print("GPT:", response.choices[0].message.content)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(debug = True)
